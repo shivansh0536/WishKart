183232604d0b1fa415f6914aa56e218df00ed49e
shivansh0536 <shivansh050906@gmail.com>
2025-07-25 04:53:47 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@
 import { useState, useEffect } from "react"
 import Navbar from "./components/navbar"
 import Footer from "./components/footer"
-import HomePage from "./components/Homepage"
+import HomePage from "./components/homepage"
 import WishlistPage from "./pages/WishlistPage"
 import RecommendationsPage from "./pages/RecommendationsPage"
 import AboutPage from "./pages/AboutPage"
